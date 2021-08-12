777a2ea627b50bdda6238b7b252f09813a22ce74
Tashfia Shamim <62017475+Tashhfia@users.noreply.github.com>
2021-08-12 18:08:09 +0800

    initializing

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,3 +8,11 @@ class AlienInvasion:
         pygame.init()
         self.screen = pygame.display.set_mode((1300, 900))
         pygame.display.set_caption("Attack of the Aliens")
+
+    def run(self):
+        while True:
+            # checking for the events
+            for event in pygame.event.get():
+                # exits the game
+                if event.type == pygame.QUIT:
+                    sys.exit()
