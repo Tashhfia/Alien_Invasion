4603a97796fab098c0ced46724c9952f2d15d6d4
Tashfia Shamim <62017475+Tashhfia@users.noreply.github.com>
2021-08-12 17:58:42 +0800

    initializing

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,10 @@
 import sys
 import pygame
 
-class  AlienInvasion:
 
+class AlienInvasion:
+    # initialize background settings
     def __init__(self):
         pygame.init()
+        self.screen = pygame.display.set_mode((1300, 900))
+        pygame.display.set_caption("Attack of the Aliens")
