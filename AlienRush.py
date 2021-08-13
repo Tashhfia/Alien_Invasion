134c9727390d01a18809ec2113db8c4076cacda7
Tashfia Shamim <62017475+Tashhfia@users.noreply.github.com>
2021-08-13 16:52:08 +0800

    shoots out aliens

--- AlienRush.py
+++ AlienRush.py
@@ -58,6 +58,10 @@ class AlienRush:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        # Check for any bullets that have hit aliens.
+        # If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
 
     def _check_events(self):
         """"Respond to keyboard and mouse events"""
